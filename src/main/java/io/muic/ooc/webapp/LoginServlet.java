ac7349d4f6dbaa2ae9f8ee48b3fb685292ea2073
Rathanon <rathanon.sin@student.mahidol.edu>
2021-06-28 16:33:43 +0700

    username and password page

--- src/main/java/io/muic/ooc/webapp/LoginServlet.java
+++ src/main/java/io/muic/ooc/webapp/LoginServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -21,8 +22,24 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
+        String error = "";
 
+        // authentication
+        if(username!= null && username.equals("mansahej") && password != null && password.equals("12345")){
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            response.sendRedirect("/");
+        } else{
+            error = "Username or password incorrect. Please try again.";
+
+            request.setAttribute("error", error);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/login.jsp");
+            requestDispatcher.include(request, response);
+        }
     }
 
 
--- src/main/webapp/WEB-INF/login.jsp
+++ src/main/webapp/WEB-INF/login.jsp
@@ -3,12 +3,17 @@
 <html>
 <title>Home page</title>
 <body>
+<p>
+        ${error}
+</p>
+<p>
 <form action="/login" method="post">
 
-        <input type="text" placeholder="Enter Username" name="uname" required>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="text" placeholder="Enter Username" name="username" required><br>
+        <input type="password" placeholder="Enter Password" name="password" required><br>
         <button type="submit">Login</button>
 
 </form>
+</p>
 </body>
 </html>
