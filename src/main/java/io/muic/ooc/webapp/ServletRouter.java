a82de75f0f83d0ccbea3e6884997b59da9387a2c
Rathanon <rathanon.sin@student.mahidol.edu>
2021-06-30 14:22:18 +0700

    Refractor code using Servlet Router

--- src/main/java/io/muic/ooc/webapp/ServletRouter.java
+++ src/main/java/io/muic/ooc/webapp/ServletRouter.java
@@ -23,21 +23,13 @@ public class ServletRouter {
                 AbstractRoutableHttpServlet httpServlet = servletClass.newInstance();
                 Tomcat.addServlet(ctx, servletClass.getSimpleName(), httpServlet);
                 // TRICK: mapping with index.jsp, allow access to root path "/"
-                ctx.addServletMapping(httpServlet.getPattern(), HomeServlet.class.getSimpleName());
+                ctx.addServletMapping(httpServlet.getPattern(), servletClass.getSimpleName());
 
             } catch (InstantiationException e) {
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-    }
-        LoginServlet loginServlet = new LoginServlet();
-        Tomcat.addServlet(ctx, LoginServlet.class.getSimpleName(), loginServlet);
-        ctx.addServletMapping("/login", LoginServlet.class.getSimpleName());
-
-        LogoutServlet logoutServlet = new LogoutServlet();
-        Tomcat.addServlet(ctx, LogoutServlet.class.getSimpleName(), logoutServlet);
-        ctx.addServletMapping("/logout", LogoutServlet.class.getSimpleName());
-
+        }
     }
 }
